a8e52863c2f8ff968fa96564d8ef96bfead52e76
evs062009 <evs062009@gmail.com>
2019-02-04 22:40:40 +0200

    add selection sort

---
+++ src/SelectionSort.java
@@ -0,0 +1,38 @@
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int [] ints = {5, 7, 1, 3, 2, 0};
+
+        System.out.print("original array:\t");
+        print(ints);
+        System.out.println();
+        sort(ints);
+        System.out.print("sorted array:\t");
+        print(ints);
+    }
+
+    private static void sort(int [] ints) {
+        for (int i = 0; i < ints.length - 1; i++) {
+            int min = ints[i];
+            int minIndex = i;
+            for (int j = i; j < ints.length; j++) {
+                if (ints[j] < min) {
+                    min = ints[j];
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                int temp = ints[minIndex];
+                ints[minIndex] = ints[i];
+                ints[i] = temp;
+            }
+        }
+    }
+
+    private static void print(int [] ints) {
+        for (int i: ints
+             ) {
+            System.out.print(i + ", ");
+        }
+    }
+}
