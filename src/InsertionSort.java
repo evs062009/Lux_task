9693d4b90fc0e689ac55ad60ad59e5d61bd6cef6
evs062009 <evs062009@gmail.com>
2019-02-05 23:29:05 +0200

    change variables names

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -3,16 +3,16 @@ import java.util.Arrays;
 public class InsertionSort {
 
     public static int [] sort(int [] ints) {
-        int [] newArr = Arrays.copyOf(ints, ints.length);
+        int [] numbers = Arrays.copyOf(ints, ints.length);
 
-        for (int i = 1; i < newArr.length; i++) {
-            int element = newArr[i];
-            int j = i;
-            for ( ; (j > 0) && (newArr[j - 1] > element); j--) {
-                newArr[j] = newArr[j - 1];
+        for (int iterCount = 1; iterCount < numbers.length; iterCount++) {
+            int element = numbers[iterCount];
+            int arrIndex = iterCount;
+            for ( ; (arrIndex > 0) && (numbers[arrIndex - 1] > element); arrIndex--) {
+                numbers[arrIndex] = numbers[arrIndex - 1];
             }
-            newArr[j] = element;
+            numbers[arrIndex] = element;
         }
-        return newArr;
+        return numbers;
     }
 }
