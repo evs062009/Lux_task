4a1583c309873536c464314a3f9d1c31acd5928b
evs062009 <evs062009@gmail.com>
2019-02-05 15:39:16 +0200

    Delete everything except sort()

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -2,17 +2,7 @@ import java.util.Arrays;
 
 public class InsertionSort {
 
-    public static void main(String[] args) {
-        int [] ints = {5, 7, 1, 3, 2, 0};
-
-        System.out.print("original array:\t");
-        System.out.println(Arrays.toString(ints));
-        int [] sortedInts = sort(ints);
-        System.out.print("sorted array:\t");
-        System.out.print(Arrays.toString(sortedInts));
-    }
-
-    private static int [] sort(int [] ints) {
+    public static int [] sort(int [] ints) {
         int [] newArr = Arrays.copyOf(ints, ints.length);
 
         for (int i = 1; i < newArr.length; i++) {
