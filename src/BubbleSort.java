41c205d0e5dd5a5e153b61237580b02615ece3d1
evs062009 <evs062009@gmail.com>
2019-02-05 23:25:08 +0200

    change variables names

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -3,15 +3,15 @@ import java.util.Arrays;
 public class BubbleSort {
 
     public static int [] sort(int [] ints) {
-        int [] newArr = Arrays.copyOf(ints, ints.length);
+        int [] numbers = Arrays.copyOf(ints, ints.length);
         boolean noSwap = true;
 
-        for (int i = newArr.length - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if (newArr[j] > newArr[j + 1]){
-                    int temp = newArr[j];
-                    newArr[j] = newArr[j + 1];
-                    newArr[j + 1] = temp;
+        for (int iterCount = numbers.length - 1; iterCount > 0; iterCount--) {
+            for (int arrIndex = 0; arrIndex < iterCount; arrIndex++) {
+                if (numbers[arrIndex] > numbers[arrIndex + 1]){
+                    int temp = numbers[arrIndex];
+                    numbers[arrIndex] = numbers[arrIndex + 1];
+                    numbers[arrIndex + 1] = temp;
                     noSwap = false;
                 }
             }
@@ -19,6 +19,6 @@ public class BubbleSort {
                 break;
             }
         }
-        return newArr;
+        return numbers;
     }
 }
