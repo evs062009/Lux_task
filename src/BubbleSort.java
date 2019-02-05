d9b3d7fb446abff544aee05d1e078b43191dbf0d
evs062009 <evs062009@gmail.com>
2019-02-05 12:11:53 +0200

    bubble sort 1st version

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,2 +1,33 @@
+import java.util.Arrays;
+
 public class BubbleSort {
+    public static void main(String[] args) {
+        int [] ints = {5, 7, 1, 3, 2, 0};
+
+        System.out.print("original array:\t");
+        System.out.println(Arrays.toString(ints));
+        int [] sortedInts = sort(ints);
+        System.out.print("sorted array:\t");
+        System.out.println(Arrays.toString(sortedInts));
+    }
+
+    private static int [] sort(int [] ints) {
+        int [] newArr = Arrays.copyOf(ints, ints.length);
+        boolean noSwap = true;
+
+        for (int i = newArr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (newArr[j] > newArr[j + 1]){
+                    int temp = newArr[j];
+                    newArr[j] = newArr[j + 1];
+                    newArr[j + 1] = temp;
+                    noSwap = false;
+                }
+            }
+            if (noSwap){
+                break;
+            }
+        }
+        return newArr;
+    }
 }
