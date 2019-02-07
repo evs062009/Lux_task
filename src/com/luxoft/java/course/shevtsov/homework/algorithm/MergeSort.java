91cc912b1278a55390aa939e9dd19900559e82e7
evs062009 <evs062009@gmail.com>
2019-02-07 09:34:14 +0200

    add fillRest()

--- src/com/luxoft/java/course/shevtsov/homework/algorithm/MergeSort.java
+++ src/com/luxoft/java/course/shevtsov/homework/algorithm/MergeSort.java
@@ -4,38 +4,31 @@ import java.util.Arrays;
 
 public class MergeSort {
 
-    public static int [] sort(int [] ints){
+    public static int[] sort(int[] ints) {
         if (ints.length > 1) {
-            int [] ints1 = Arrays.copyOfRange(ints, 0, ints.length / 2);
-            int [] ints2 = Arrays.copyOfRange(ints, ints1.length, ints.length);
+            int[] ints1 = Arrays.copyOfRange(ints, 0, ints.length / 2);
+            int[] ints2 = Arrays.copyOfRange(ints, ints1.length, ints.length);
             return getSortedArr(ints1, ints2);
         }
         return ints;
     }
 
-    private static int [] getSortedArr(int [] ints1, int [] ints2){
-        int [] result = new int [ints1.length + ints2.length];
+    private static int[] getSortedArr(int[] ints1, int[] ints2) {
+        int[] result = new int[ints1.length + ints2.length];
         ints1 = sort(ints1);
         ints2 = sort(ints2);
 
         for (int resultIndex = 0, ints1Index = 0, ints2Index = 0; resultIndex < result.length; resultIndex++) {
-
-            if (ints1Index == ints1.length){
-                for (int fillIndex = resultIndex; fillIndex < result.length; fillIndex++, ints2Index++) {
-                    result[fillIndex] = ints2[ints2Index];
-                }
+            if (ints1Index == ints1.length) {
+                fillRest(result, resultIndex, ints2, ints2Index);
+                break;
+            }
+            if (ints2Index == ints2.length) {
+                fillRest(result, resultIndex, ints1, ints1Index);
                 break;
             }
 
-            //объединить с предыдущим в 1 метод
-            if (ints2Index == ints2.length){
-                for (int fillIndex = resultIndex; fillIndex < result.length; fillIndex++, ints1Index++) {
-                    result[fillIndex] = ints1[ints1Index];
-                }
-                break;
-            }
-
-            if (ints1[ints1Index] < ints2[ints2Index]){
+            if (ints1[ints1Index] < ints2[ints2Index]) {
                 result[resultIndex] = ints1[ints1Index];
                 ints1Index++;
             } else {
@@ -44,5 +37,13 @@ public class MergeSort {
             }
         }
         return result;
+    }
+
+    private static void fillRest(int[] destInts, int destFrom, int[] sourceInts, int sourceFrom) {
+        int destIndex = destFrom;
+        int sourceIndex = sourceFrom;
+        for (; destIndex < destInts.length; destIndex++, sourceIndex++) {
+            destInts[destIndex] = sourceInts[sourceIndex];
+        }
     }
 }
