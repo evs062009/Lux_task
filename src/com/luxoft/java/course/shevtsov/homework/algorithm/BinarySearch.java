2d5b512a56e9efd45b77a1c2a129cde29dac6aea
evs062009 <evs062009@gmail.com>
2019-02-07 13:23:07 +0200

    working ver2 with auto test in Main

--- src/com/luxoft/java/course/shevtsov/homework/algorithm/BinarySearch.java
+++ src/com/luxoft/java/course/shevtsov/homework/algorithm/BinarySearch.java
@@ -3,45 +3,34 @@ package com.luxoft.java.course.shevtsov.homework.algorithm;
 import com.luxoft.java.course.shevtsov.homework.myUtilities.MyUtilities;
 
 public class BinarySearch {
-    private static Integer resultIndex = null;
 
     public static Integer search(int[] ints, int searchNumber) {
         if (ints.length > 0 && !MyUtilities.ifArrSotredByAsc(ints)) {
             System.out.println("Invalid parameters. The Array is empty or not sorted by ascending.");
             return null;
         }
-        binarySearch(ints, 0, ints.length - 1, searchNumber);
-        return resultIndex;
+        return binarySearch(ints, 0, ints.length - 1, searchNumber);
     }
 
-    private static void binarySearch(int[] ints, int from, int to, int searchNumber) {
-        if (from == to) {
-            if (ints[from] == searchNumber) {
-                resultIndex = from;
-            } else {
-                System.out.println("There is NO such element.");
-            }
-            return;
-        }
+    private static Integer binarySearch(int[] ints, int from, int to, int searchNumber) {
+        Integer resultIndex = null;
 
-        int baseIndex = from + ((to - from) / 2);
-
-        if (ints[baseIndex] == searchNumber) {
-            resultIndex = baseIndex;
-        } else if (ints[baseIndex] > searchNumber) {
-            to = baseIndex - 1;
-            if (searchNumber > ints[to]){
-                System.out.println("There is NO such element.");
+        if (searchNumber >= ints[from] && searchNumber <= ints[to]) {
+            if (from == to) {
+                if (ints[from] == searchNumber) {
+                    resultIndex = from;
+                }
             } else {
-                binarySearch(ints, from, to, searchNumber);
-            }
-        } else {
-            from = baseIndex + 1;
-            if (searchNumber < ints[from]){
-                System.out.println("There is NO such element.");
-            } else {
-                binarySearch(ints, from, to, searchNumber);
+                int baseIndex = from + ((to - from) / 2);
+                if (ints[baseIndex] == searchNumber) {
+                    resultIndex = baseIndex;
+                } else if (ints[baseIndex] > searchNumber) {
+                    resultIndex =  binarySearch(ints, from, baseIndex - 1, searchNumber);
+                } else {
+                    resultIndex = binarySearch(ints, baseIndex + 1, to, searchNumber);
+                }
             }
         }
+        return resultIndex;
     }
 }
--- src/com/luxoft/java/course/shevtsov/homework/algorithm/Main.java
+++ src/com/luxoft/java/course/shevtsov/homework/algorithm/Main.java
@@ -1,19 +1,23 @@
 package com.luxoft.java.course.shevtsov.homework.algorithm;
 
-import com.luxoft.java.course.shevtsov.homework.myUtilities.MyUtilities;
-
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        int [] ints = {-10, -8, -4, 0, 1, 5, 6, 9, 12, 20};
-
+        int[] ints = {-10, -8, -4, 0, 1, 5, 6, 9, 12, 20};
+        Integer resultIndex = null;
         System.out.println("Array for searching:\t" + Arrays.toString(ints));
-        int searchNumber = MyUtilities.inputInt();
-        Integer resultIndex = BinarySearch.search(ints, searchNumber);
-        if (resultIndex != null) {
-            System.out.println("index of '" + searchNumber + "' element is " +resultIndex);
+
+        for (int i = -11; i < 22; i++) {
+            System.out.println("-----------------------------------------------------------------");
+            System.out.println("search for " + i);
+            resultIndex = BinarySearch.search(ints, i);
+            if (resultIndex == null) {
+                System.out.println("There is NO such element.");
+            } else {
+                System.out.println("index of '" + i + "' element is " + resultIndex);
+            }
         }
 
         //написать реадми
