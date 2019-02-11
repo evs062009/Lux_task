902f4c3312eda387f0797159ce39e7f3194f3cc1
evs062009 <evs062009@gmail.com>
2019-02-11 10:18:57 +0200

    del state, del int->string

--- src/com/luxoft/java/course/shevtsov/homework/algorithm/additional/NumberSymmetryCheck.java
+++ src/com/luxoft/java/course/shevtsov/homework/algorithm/additional/NumberSymmetryCheck.java
@@ -1,30 +1,30 @@
 package com.luxoft.java.course.shevtsov.homework.algorithm.additional;
 
 public class NumberSymmetryCheck {
-    private final static int NUMBER_HALF_LENGTH = 2;
-    public final static int NUMBER_LENGTH = NUMBER_HALF_LENGTH * 2;
-    private static int number1 = 0;
 
-    public static boolean checkNumberSymmetry(int number){
-        number1 = Math.abs(number);
-        String strNumber = "" + number1;
+    public static boolean checkNumberSymmetry(int number) {
+        int number_half_length = 2;
+        int number_length = number_half_length * 2;
+        int number1 = Math.abs(number);
 
-        if (strNumber.length() <= NUMBER_LENGTH){
-            int sumRightHalf = countSumOfHalfCharacters();
-            int sumLeftHalf = countSumOfHalfCharacters();
+        if (number1 <= 9999) {
+            int sumRightHalf = countSumOfCharacters(number1, number_half_length, number_length);
+            int sumLeftHalf = countSumOfCharacters(number1, 0, number_half_length);
             return sumRightHalf == sumLeftHalf;
         } else {
-            System.out.println("Invalid parameter! Number length should not exceed " + NUMBER_LENGTH + " characters");
+            System.out.println("Invalid parameter! Number length should not exceed " + number_length + " characters");
             return false;
         }
     }
 
-    private static int countSumOfHalfCharacters(){
+    private static int countSumOfCharacters(int number, int fromRegister, int toRegister) {
         int sum = 0;
 
-        for (int i = 0; i < NUMBER_HALF_LENGTH; i++) {
-            sum += number1 % 10;
-            number1 /= 10;
+        for (int register = 0; register < toRegister; register++) {
+            if (register >= fromRegister && register < toRegister) {
+                sum += number % 10;
+            }
+            number /= 10;
         }
         return sum;
     }
